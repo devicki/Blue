437e414d9dfc79686bd4241538e670343a4a2bf6
Kasper Devicki <kasper.devicki@gmail.com>
2018-04-18 13:30:19 +0200

    added files

---
+++ js/menu-toggle.js
@@ -0,0 +1,8 @@
+function showOrHide() {
+	let toggler = document.getElementById('navtoggle');
+	if (toggler.style.display == 'block') {
+		toggler.style.display = 'none';
+	} else {
+		toggler.style.display = 'block';
+	}
+}
